0757d4be11821e8b6b855b6fd3e59b97efac7b65
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-04-06 17:51:41 -0700

    backend commit

--- server/routes/recipes-route.js
+++ server/routes/recipes-route.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const recipesController = require("../controllers/recipes-controller");
 
-router.route("/").get(recipesController.recipesList);
+router.route("/")
+    .get(recipesController.recipesList)
+    .post(recipesController.createRecipe)
 
-router.route("/:id").get(recipesController.recipeById);
+router.route("/:id")
+    .get(recipesController.recipeById)
+    .put(recipesController.editRecipeById)
+    .delete(recipesController.deleteRecipe)
+
 
 module.exports = router;
